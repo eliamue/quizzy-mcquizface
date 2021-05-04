aa05a879d0e9685aaf798316269175957f239bf5
Elise Muellerleile <eliamue@live.com>
2021-05-04 13:02:58 -0500

    fixed false positive and header overflow, added new font

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //export functions
 import { isYes } from './utils.js';
+import { isNo } from './utils.js';
 
 //DOM elements
 const button = document.getElementById('boaty-button');
@@ -24,20 +25,30 @@ button.addEventListener('click', () => {
 //Question 1
     const answer1 = prompt('Okay, was Boaty McBoatface the first viral example of an official naming contest resulting in a "Something McSomething" winning name? y/n');
     let score = 0;
-    if (!isYes(answer1)) {
+    if (isYes(answer1)) {
+        score = score + 0;
+    }
+    if (isNo(answer1)) {
         score = score + 1;
     }
+
 
 //Question 2
     const answer2 = prompt('Was the name Boaty McBoatface actually used for anything official? y/n');
     if (isYes(answer2)) {
         score = score + 1;
     }
+    if (isNo(answer1)) {
+        score = score + 0;
+    }
 
 //Question 3
     const answer3 = prompt('Will Boaty be used for anything vital? y/n');
     if (isYes(answer3)) {
         score = score + 1;
+    }
+    if (isNo(answer1)) {
+        score = score + 0;
     }
 
 //results
--- utils.js
+++ utils.js
@@ -2,5 +2,9 @@ export function isYes(userInput) {
     const firstLetter = userInput[0].toLowerCase();
 
     return firstLetter === 'y' ? true : false;
+}
+export function isNo(userInput) {
+    const firstLetter = userInput[0].toLowerCase();
 
+    return firstLetter === 'n' ? true : false;
 }
